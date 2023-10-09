e167858c6dfc24d3f9b3e68f14ddbc5c58648faa
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-10-09 19:19:58 +0300

    Update mainPage.js

--- projects/loft-photo/mainPage.js
+++ projects/loft-photo/mainPage.js
@@ -1,1 +1,99 @@
+const PERM_FRIENDS = 2;
+const PERM_PHOTOS = 4;
+const APP_ID = 138184625;
 
+export default {
+
+login() {
+  return new Promise((resolve, reject) => {
+    VK.init({
+      apiId: APP_ID,
+    });
+  
+    VK.Auth.login((response) => {
+      if (response.session) {
+        resolve(response);
+      } else {
+        console.error(response);
+        reject(response);
+      }
+    }, PERM_FRIENDS | PERM_PHOTOS);
+  });
+},
+
+async getNextPhoto() {
+  const friend = this.getRandomElement(this.friends.items);
+  const photos = await this.getFriendPhotos(friend.id);
+  const photo = this.getRandomElement(photos.items); 
+  const size = this.findSize(photo);
+
+  return { friend, id: photo.id, url: size.url };
+},
+
+async init() {
+  this.photoCache = {};
+  this.friends = await this.getFriends();
+},
+
+findSize(photo) {
+  const size = photo.sizes.find((size) => size.width >= 360);
+
+  if (!size) {
+    return photo.sizes.reduce((biggest, current) => {
+      if (current.width > biggest.width) {
+        return current;
+      }
+
+      return biggest;
+    } photo.sizes[0]);
+  }
+
+  return size;
+},
+
+photoCache: {},
+
+callApi(method, params) {
+  params.v = params.v || '5.120';
+
+  return new Promise(resolve, reject => {
+    VK.api(method, params, (response) => {
+      if (response.error) {
+        reject(new Error(response.error.error_msg));
+      } else {
+        resolve(response.response);
+      }
+    });
+  });
+},
+
+getFriends() {
+  const params = {
+    fields: ['photo_50', 'photo_100'],
+  };
+
+  return this.callApi('friends.get', params);
+},
+
+getPhotos(owner) {
+  const params = {
+    owner_id: owner,
+  };
+
+  return this.callApi('photos.getAll', params);
+},
+
+async getFriendPhotos(id) {
+  let photos = this.photoCache[id];
+
+  if (photos) {
+    return photos;
+  }
+
+  photos = await this.getPhotos(id);
+
+  this.photoCache[id] = photos;
+
+  return photos;
+},
+}
