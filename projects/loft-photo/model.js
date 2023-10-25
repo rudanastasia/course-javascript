da2c25a6246aacd33844a706f8aad2cba2eb09a5
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-10-25 19:13:48 +0300

    Update model.js

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -122,4 +122,46 @@ getUsers(ids) {
 
   return this.callApi('users.get', params);
 },
+
+async callServer (method, queryParams, body) {
+  queryParams = {
+    ...queryParams,
+    method,
+  };
+  const query = Object.entries(queryParams)
+  .reduce((all, [name, value]) => {
+    all.push(`${name}=${encodeURIComponent(value)}`);
+    return all;
+  }, [])
+  .join('&');
+  const params = {
+    headers: {
+      vk_token: this.token,
+    },
+  };
+
+  if (body) {
+    params.method = 'POST';
+    params.body = JSON.stringify(body);
+  }
+
+  const response = await fetch(`/loft-photo/api/?&{query}`, params);
+  return response.json();
+},
+
+async like(photo) {
+  return this.callServer('like', {photo});
+},
+
+async photoStats(photo) {
+  return this.callServer('photoStats', {photo});
+},
+
+async getComments(photo) {
+  return this.callServer('getComments', {photo});
+},
+
+async postComment(photo, text) {
+  return this.callServer('postComment', {photo}, {text});
+},
 }
