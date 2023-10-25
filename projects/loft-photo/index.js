840646fc1ed026f110cd3506667e501e2f658be2
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-10-25 19:13:11 +0300

    Update index.js

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,13 +1,18 @@
 import pages from './pages';
 import model from './model';
 import profilePage from './profilePage';
+import commentsTemplate from './commentsTemplate.html.hbs';
 import('./styles.css');
+
 
 /*const pageNames = ['login', 'main', 'profile'];
 document.addEventListener('click', () => {
     const pageName = model.getRandomElement(pageNames);
     pages.openPage(pageName);
 });*/
+const commentsElements = commentsTemplate({ list: comments });
+listElement.appendChild(commentsElements);
+
 
 import mainPage from './mainPage';
 import loginPage from './loginPage';
