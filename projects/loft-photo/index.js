574948fba101c3d079df335acfdb685088c50885
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-10-28 11:18:04 +0300

    Update index.js

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,7 +1,6 @@
 import pages from './pages';
 import model from './model';
 import profilePage from './profilePage';
-import commentsTemplate from './commentsTemplate.html.hbs';
 import('./styles.css');
 
 
@@ -10,8 +9,7 @@ document.addEventListener('click', () => {
     const pageName = model.getRandomElement(pageNames);
     pages.openPage(pageName);
 });*/
-const commentsElements = commentsTemplate({ list: comments });
-listElement.appendChild(commentsElements);
+
 
 
 import mainPage from './mainPage';
